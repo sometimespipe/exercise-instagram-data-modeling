7d2781cca2990197fb1e2b01da7af29eb7de484c
sometimespipe <andrespipehl@gmail.com>
2021-04-07 02:46:22 +0000

    added classes for instagram data model

--- src/models.py
+++ src/models.py
@@ -8,26 +8,62 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+# User, Post, Comment, Like, hashtags, save, page_surfing
+
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    age = Column(Integer, nullable=False)
+    email = Column(String(250), nullable=False)
+   
 
-class Address(Base):
-    __tablename__ = 'address'
+class Post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    caption = Column(String(250))
+    location = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     def to_dict(self):
         return {}
+
+class Comment(Base):
+    __tablename__ = "comment"
+    id = Column(Integer, primary_key=True)
+    content_post = relationship(Post)
+    likes_id = Column(Integer, ForeignKey('user.id'))
+    responses_id = Column(String(250), ForeignKey('user.id'))
+
+class Like(Base):
+    __tablename__ = "like"
+    id = Column(Integer, primary_key=True)
+    like_post = relationship(Post)
+    like_by_user = Column(Integer, ForeignKey('post.id'))
+    like_id = relationship(Post)
+
+class Hashtag(Base):
+    __tablename__ = "hashtag"
+    id = Column(Integer, primary_key=True)
+    depiction = Column(String(250), nullable=False)
+    hashtag_parent = relationship(Post)
+    created_by_user = Column(String(250), ForeignKey('post.id'))
+    like_id = relationship(Post)
+
+class Save(Base):
+    __tablename__ = "save"
+    id = Column(Integer, primary_key=True)
+    save_post = relationship(Post)
+    saved_by_user = Column(Integer, ForeignKey('post.id'))
+    save_id = relationship(Post)
+    
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
